e92562857298e244926e78190a2d9608ea68fd3c
harshbutfair <harsh.narayan@yahoo.com>
2021-06-30 02:20:32 +0530

    Added arrow functions

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ document.getElementById('button3').addEventListener('click', getExternal)
 
 function getText(){
     fetch('text.txt')
-        .then(function(response){
-            return response.text();
-        })
-        .then(function(data){
+        // .then(function(response){
+        //     return response.text();
+        // })
+        .then(response => response.text())
+        // .then(function(data){
+        .then(data =>{
             document.getElementById('output').innerHTML = data;
         })
-        .catch(function(err){
-            console.log(err);
-        })
+        // .catch(function(err){
+        //     console.log(err);
+        // })
+        .catch(err => console.log(err));
 }
 
 function getJson(){
     fetch('posts.json')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
+        // .then(function(response){
+        //     return response.json();
+        // })
+        .then(response => response.json())
+        // .then(function(data){
+        .then(data => {
             let output = '';
             data.forEach(function(post){
                 output += `<li>${post.title}</li>`
@@ -34,17 +39,19 @@ function getJson(){
 
 function getExternal(){
     fetch('https://api.github.com/users')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
+        // .then(function(response){
+        //     return response.json();
+        // })
+        .then(response => response.json())
+        .then(data => {
             let output = '';
             data.forEach(function(post){
                 output += `<li>${post.login}</li>`
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function(err){
-            console.log(err);
-        })
+        // .catch(function(err){
+        //     console.log(err);
+        // })
+        .catch(err => console.log(err))
 }
