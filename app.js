deccb2ebd71a2ba998a6750cc5a480c5692e8c2a
harshbutfair <harsh.narayan@yahoo.com>
2021-06-30 00:50:11 +0530

    Added get API using fetch

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById('button1').addEventListener('click', getText)
 
 document.getElementById('button2').addEventListener('click', getJson)
 
+document.getElementById('button3').addEventListener('click', getExternal)
+
 
 function getText(){
     fetch('text.txt')
@@ -27,5 +29,22 @@ function getJson(){
                 output += `<li>${post.title}</li>`
             });
             document.getElementById('output').innerHTML = output;
+        })
+}
+
+function getExternal(){
+    fetch('https://api.github.com/users')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let output = '';
+            data.forEach(function(post){
+                output += `<li>${post.login}</li>`
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(function(err){
+            console.log(err);
         })
 }
